519ebf045523a9b09f3a799c6f742b07b760c659
garreith <garreithwihary@gmail.com>
2026-02-25 16:26:17 +0700

    msg

---
+++ simple_youtube/src/App.js
@@ -0,0 +1,143 @@
+import logo from './logo.svg';
+import './App.css';
+import { use, useEffect, useState } from 'react'
+
+
+function App() {
+  const [tf, setTF] = useState(false)
+
+  const [tf2, setTF2] = useState(false)
+
+  const [color_user_1, setC1] = useState("profileG")
+
+  const [user1, setU1] = useState("Garreith")
+
+  const [user2, setU2] = useState("username_1")
+
+  const [user3, setU3] = useState("username_2")
+
+  const [icon, setI] = useState("G")
+
+
+  function enable_change_profile(){
+    setTF(true) 
+  }
+
+  function change_profile2(){
+    if (user2 == "username_1"){
+      if (user1 == "Garreith"){
+        setU2("Garreith")
+      }
+      else{
+        setU2("username_2")
+      }
+      setU1("username_1")
+      setI("1")
+      setC1("profile1")
+    }
+    if (user2 == "username_2"){
+      if (user1 == "Garreith"){
+        setU2("Garreith")
+      }
+      else{
+        setU2("username_1")
+      }
+      setU1("username_2")
+      setI("2")
+      setC1("profile2")
+    }
+    if (user2 == "Garreith"){
+      if (user1 == "username_1"){
+        setU2("username_1")
+      }
+      else{
+        setU2("username_2")
+      }
+      setU1("Garreith")
+      setI("G")
+      setC1("profileG")
+    }
+    setTF(false)
+  }
+
+  function change_profile3(){
+    if (user3 == "username_1"){
+      if (user1 == "Garreith"){
+        setU3("Garreith")
+      }
+      else{
+        setU3("username_2")
+      }
+      setU1("username_1")
+      setI("1")
+      setC1("profile1")
+    }
+    if (user3 == "username_2"){
+      if (user1 == "Garreith"){
+        setU3("Garreith")
+      }
+      else{
+        setU3("username_1")
+      }
+      setU1("username_2")
+      setI("2")
+      setC1("profile2")
+    }
+    if (user3 == "Garreith"){
+      if (user1 == "username_1"){
+        setU3("username_1")
+      }
+      else{
+        setU3("username_2")
+      }
+      setU1("Garreith")
+      setI("G")
+      setC1("profileG")
+    }
+    setTF(false)
+  }
+
+  function profile_click(){
+    setTF2(true)
+  }
+
+  return (
+    <div>
+        <button className='b1'>▶</button>
+        <hr className='hr1'></hr>
+        <label className='lb1'>video title</label>
+        <button className='b0' onClick={profile_click}></button>
+        <label className='lb2'>Username_ABC</label>
+        <label className='lb3'>999M subscribers</label>
+        <button className='b2'>Subscribe</button>
+        <button className='b3'>👍 100M</button>
+        <button className='b4'>👎</button>
+        <button className='b5'>↪ Share</button>
+        <button className='b6'>⛉ Save</button>
+        <button className='b7'>⇩ Download</button>
+        <button className='b8'>...</button>
+        <button className={color_user_1} onClick={enable_change_profile}>{icon}</button>
+        {tf === true? 
+        <div className='div1'>
+          <button onClick={change_profile2}>{user2}</button>
+          <br></br>
+          <button onClick={change_profile3}>{user3}</button>
+        </div>
+        :<div></div>}
+        {tf2 === true? 
+        <div className='div2'>
+          <label>steam</label>
+          <img src={logo}></img>
+          <br></br>
+          <label>nintendo</label>
+          <img src={logo}></img>
+          <br></br>
+          <label>sony</label>
+          <img src={logo}></img>
+        </div>
+        :<div></div>}
+    </div>
+  );
+}
+
+export default App;
