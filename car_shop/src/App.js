de1c00f4084a294126522b0430d79d451ece8bbb
garreith <garreithwihary@gmail.com>
2026-06-10 16:12:56 +0700

    text

--- car_shop/src/App.js
+++ car_shop/src/App.js
@@ -1,184 +1,338 @@
-import logo from './logo.svg';
 import './App.css';
-import { use, useState } from 'react';
+import { useState } from 'react';
 
 function App() {
 
-  const [selected, setSelected] = useState('brand')
+  const [selected, setSelected] = useState('brand');
 
-  const [selected2, setSelected2] = useState('Honda')
+  const [selected2, setSelected2] = useState('Honda');
 
-  const [showList, setShowList] = useState(['Honda', 'Toyota', 'Mazda'])
+  const brandList = ['Honda', 'Toyota', 'Mazda'];
 
-  const [brandList, setBrandList] = useState(['Honda', 'Toyota', 'Mazda'])
+  const typeList = ['Hybrid', 'Electric'];
 
-  const [typeList, setTypeList] = useState(['Hybrid', 'Electric'])
+  const priceList = [
+    '$15000 - $30000',
+    '$30000 - $50000',
+    '$50000 - $100000'
+  ];
 
-  const [priceList, setPriceList] = useState(['$15000 - $30000', '$30000 - $50000', '$50000 - $100000'])
+  const colorList = ['White', 'Black', 'Red'];
 
-  const [car_list, setCarList] = useState([
-    {"file" : "HRV.png", "price" : 24000, "stock" : 15, "amount" : 0, "index" : "0", "brand" : "Honda", "type" : "Hybrid"},
-    {"file" : "Innova.png", "price" : 26000, "stock" : 20, "amount" : 0, "index" : "1", "brand" : "Toyota", "type" : "Electric"},
-    {"file" : "CX-5.png", "price" : 40000, "stock" : 10, "amount" : 0, "index" : "2", "brand" : "Mazda", "type" : "Hybrid"}
-  ])
+  const [showList, setShowList] = useState(brandList);
 
-  const [car_list2, setCarList2] = useState([
-    {"file" : "HRV.png", "price" : 24000, "stock" : 15, "amount" : 0, "index" : "0", "brand" : "Honda", "type" : "Hybrid"},
-    {"file" : "Innova.png", "price" : 26000, "stock" : 20, "amount" : 0, "index" : "1", "brand" : "Toyota", "type" : "Electric"},
-    {"file" : "CX-5.png", "price" : 40000, "stock" : 10, "amount" : 0, "index" : "2", "brand" : "Mazda", "type" : "Hybrid"}
-  ])
+  const [carList, setCarList] = useState([
+    {
+      file: 'HRV.png',
+      price: 24000,
+      stock: 15,
+      amount: 0,
+      brand: 'Honda',
+      type: 'Hybrid',
+      color: 'White'
+    },
+    {
+      file: 'Innova.png',
+      price: 26000,
+      stock: 20,
+      amount: 0,
+      brand: 'Toyota',
+      type: 'Electric',
+      color: 'Black'
+    },
+    {
+      file: 'CX-5.png',
+      price: 40000,
+      stock: 10,
+      amount: 0,
+      brand: 'Mazda',
+      type: 'Hybrid',
+      color: 'Red'
+    }
+  ]);
 
-  function Car(props){
+  const [filteredCars, setFilteredCars] = useState(carList);
 
-    function add(){
-      let tempList = []
+  const [totalPrice, setTotalPrice] = useState(0)
 
-      for (let x = 0; x < car_list.length; x++){
-        tempList.push(car_list[x])
-      }
+  function Car(props) {
 
-      if (props.index === "0"){
-        tempList[0].amount += 1
-      }
-      else if (props.index === "1"){
-        tempList[1].amount += 1
-      }
-      else if (props.index === "2"){
-        tempList[2].amount += 1
-      }
+    function add() {
 
-      setCarList(tempList)
+      const updated = carList.map((car, index) => {
+
+        if (index === props.index) {
+          return {
+            ...car,
+            amount: car.amount + 1
+          };
+        }
+
+        return car;
+      });
+
+      setCarList(updated);
+      setFilteredCars(updated);
     }
 
-    function minus(){
-      let tempList = []
+    function minus() {
 
-      for (let x = 0; x < car_list.length; x++){
-        tempList.push(car_list[x])
-      }
+      const updated = carList.map((car, index) => {
 
-      if (props.index === "0" && props.count !== 0){
-        tempList[0].amount -= 1
-      }
-      else if (props.index === "1" && props.count !== 0){
-        tempList[1].amount -= 1
-      }
-      else if (props.index === "2" && props.count !== 0){
-        tempList[2].amount -= 1
-      }
+        if (index === props.index && car.amount > 0) {
+          return {
+            ...car,
+            amount: car.amount - 1
+          };
+        }
 
-      setCarList(tempList)
+        return car;
+      });
+
+      setCarList(updated);
+      setFilteredCars(updated);
     }
 
-    function cart(){
-      let tempList = []
+    function cart() {
 
-      for (let x = 0; x < car_list.length; x++){
-        tempList.push(car_list[x])
-      }
+      const updated = carList.map((car, index) => {
 
-      if (props.index === "0"){
-        if (props.count <= props.stock){
-          tempList[0].stock = tempList[0].stock - tempList[0].amount
-        }
-        else{
-          alert("Not enough stock for HRV")
-        }
-      }
-      else if (props.index === "1"){
-        if (props.count <= props.stock){
-          tempList[1].stock = tempList[1].stock - tempList[1].amount
-        }
-        else{
-          alert("Not enough stock for Innova")
-        }
-      }
-      else if (props.index === "2"){
-        if (props.count <= props.stock){
-          tempList[2].stock = tempList[2].stock - tempList[2].amount
-        }
-        else{
-          alert("Not enough stock for CX-5")
-        }
-      }
+        if (index === props.index) {
 
-      setCarList(tempList)
+          if (car.amount > car.stock) {
+            alert(`Not enough stock for ${car.brand}`);
+            return car;
+          }
+
+          return {
+            ...car,
+            stock: car.stock - car.amount,
+            amount: 0,
+          };
+        }
+
+        return car;
+      });
+      
+      setTotalPrice(totalPrice + props.price * props.count)
+      console.log(totalPrice)
+      setCarList(updated);
+      setFilteredCars(updated);
     }
 
     return (
       <div className='div1'>
-        <img src={props.file} className='img1'></img>
-        <br></br>
-        <label className='label1'>Brand: {props.brand}</label>
-        <br></br>
-        <label className='label1'>Type  : {props.type}</label>
-        <br></br>
-        <label className='label1'>Price : {props.price}</label>
-        <br></br>
-        <label className='label1'>Stock: {props.stock}</label>
-        <input className='input1' value={props.count}></input>
-        <button className='button1' onClick={add}>▲</button>
-        <button className='button2' onClick={minus}>▼</button>
-        <button className='button3' onClick={cart}>add to cart</button>
-        
+
+        <img src={props.file} className='img1' alt='' />
+
+        <br />
+
+        <label className='label1'>
+          Brand: {props.brand}
+        </label>
+
+        <br />
+
+        <label className='label1'>
+          Type: {props.type}
+        </label>
+
+        <br />
+
+        <label className='label1'>
+          Price: ${props.price}
+        </label>
+
+        <br />
+
+        <label className='label1'>
+          Stock: {props.stock}
+        </label>
+
+        <br />
+
+        <label className='label1'>
+          Color: {props.color}
+        </label>
+
+        <br />
+
+        <input
+          className='input1'
+          value={props.count}
+          readOnly
+        />
+
+        <button className='button1' onClick={add}>
+          ▲
+        </button>
+
+        <button className='button2' onClick={minus}>
+          ▼
+        </button>
+
+        <button className='button3' onClick={cart}>
+          add to cart
+        </button>
+
       </div>
-    )
+    );
   }
 
-  function select(e){
-    setSelected(e.target.value)
-    if (e.target.value === 'brand'){
-      setShowList(brandList)
+  function select(e) {
+
+    const value = e.target.value;
+
+    setSelected(value);
+
+    if (value === 'brand') {
+      setShowList(brandList);
+      setSelected2(brandList[0]);
     }
-    else if (e.target.value === 'price'){
-      setShowList(priceList)
+    else if (value === 'price') {
+      setShowList(priceList);
+      setSelected2(priceList[0]);
     }
-    else if (e.target.value === 'type'){
-      setShowList(typeList)
+    else if (value === 'type') {
+      setShowList(typeList);
+      setSelected2(typeList[0]);
+    }
+    else if (value === 'color') {
+      setShowList(colorList);
+      setSelected2(colorList[0]);
     }
   }
 
-  function search(){
-    let tempList = []
+  function search() {
 
-    if (selected === 'brand'){
-      tempList = car_list.filter(item => item.brand === selected2)
+    let tempList = [];
+
+    if (selected === 'brand') {
+
+      tempList = carList.filter(
+        item => item.brand === selected2
+      );
     }
-    else if (selected === 'type'){
-      tempList = car_list.filter(item => item.type === selected2)
+
+    else if (selected === 'type') {
+
+      tempList = carList.filter(
+        item => item.type === selected2
+      );
     }
-    else if (selected === 'price'){
-      if (selected2 === '$15000 - $30000'){
-        tempList = car_list.filter(item => {return item.price >= 15000 && item.price <= 30000})
+
+    else if (selected === 'price') {
+
+      if (selected2 === '$15000 - $30000') {
+
+        tempList = carList.filter(
+          item => item.price >= 15000 && item.price <= 30000
+        );
+      }
+
+      else if (selected2 === '$30000 - $50000') {
+
+        tempList = carList.filter(
+          item => item.price >= 30000 && item.price <= 50000
+        );
+      }
+
+      else if (selected2 === '$50000 - $100000') {
+
+        tempList = carList.filter(
+          item => item.price >= 50000 && item.price <= 100000
+        );
       }
     }
 
-    setCarList2(tempList)
+    else if (selected === 'color') {
+
+      tempList = carList.filter(
+        item => item.color === selected2
+      );
+    }
+
+    setFilteredCars(tempList);
   }
 
-  function reset(){
-    setCarList2(car_list)
+  function reset() {
+    setFilteredCars(carList);
   }
 
   return (
+
     <div className="App">
-      <select value={selected} onChange={select} className='select1'>
+
+      <select
+        value={selected}
+        onChange={select}
+        className='select1'
+      >
+
         <option value="brand">Brand</option>
+
         <option value="type">Type</option>
+
         <option value="price">Price</option>
+
+        <option value="color">Color</option>
+
       </select>
-      <br></br>
-      <select value={selected2} onChange={(e) => setSelected2(e.target.value)} className='select2'>
-        <option value={showList[0]}>{showList[0]}</option>
-        <option value={showList[1]}>{showList[1]}</option>
-        <option value={showList[2]}>{showList[2]}</option>
+
+      <br />
+
+      <select
+        value={selected2}
+        onChange={(e) => setSelected2(e.target.value)}
+        className='select2'
+      >
+
+        {
+          showList.map(item => (
+            <option key={item} value={item}>
+              {item}
+            </option>
+          ))
+        }
+
       </select>
-      <br></br>
-      <button className='button4' onClick={search}>search</button>
-      <button className='button5' onClick={reset}>reset filters</button>
+
+      <br />
+
+      <button
+        className='button4'
+        onClick={search}
+      >
+        search
+      </button>
+
+      <button
+        className='button5'
+        onClick={reset}
+      >
+        reset filters
+      </button>
+
       {
-        car_list2.map(cars => <Car file={cars.file} price={"$" + cars.price} stock={cars.stock} count={cars.amount} index={cars.index} brand={cars.brand} type={cars.type} />)
+        filteredCars.map((cars, index) => (
+
+          <Car
+            key={index}
+            index={index}
+            file={cars.file}
+            price={cars.price}
+            stock={cars.stock}
+            count={cars.amount}
+            brand={cars.brand}
+            type={cars.type}
+            color={cars.color}
+          />
+
+        ))
       }
+
+      <label className='total'>Total Price: ${totalPrice}</label>
+
     </div>
   );
 }
